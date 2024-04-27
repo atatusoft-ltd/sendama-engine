f50afe5308dbd22d56ba61d5412b7dd1dbe849c0
Andrew Masiye <amasiye313@gmail.com>
2024-04-28 00:50:25 +0200

    feat(events): create SceneEventType.php
    - Implemented KeyboardEvent.php methods
    - Implement SceneEvent.php

--- src/Events/SceneEvent.php
+++ src/Events/SceneEvent.php
@@ -2,7 +2,22 @@
 
 namespace Sendama\Engine\Events;
 
+use DateTimeImmutable;
+use DateTimeInterface;
+use Sendama\Engine\Core\Interfaces\SceneInterface;
+use Sendama\Engine\Events\Enumerations\EventType;
+use Sendama\Engine\Events\Enumerations\SceneEventType;
+use Sendama\Engine\Events\Interfaces\EventTargetInterface;
+
 readonly class SceneEvent extends Event
 {
-
+  public function __construct(
+    public SceneEventType $sceneEventType,
+    public ?SceneInterface $scene = null,
+    ?EventTargetInterface $target = null,
+    DateTimeInterface $timestamp = new DateTimeImmutable()
+  )
+  {
+    parent::__construct(EventType::SCENE, $target, $timestamp);
+  }
 }
