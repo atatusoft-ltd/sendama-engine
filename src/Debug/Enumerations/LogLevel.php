b993d0b30ea8b1ecd33230f939dab30f10ecbd21
Andrew Masiye <amasiye313@gmail.com>
2024-04-28 00:46:44 +0200

    feat(debug): add loglevel debugging

--- src/Debug/Enumerations/LogLevel.php
+++ src/Debug/Enumerations/LogLevel.php
@@ -12,4 +12,20 @@ enum LogLevel: string
   case WARN = 'warn';
   case ERROR = 'error';
   case FATAL = 'fatal';
+
+  /**
+   * Returns the priority of the log level.
+   *
+   * @return int The priority of the log level.
+   */
+  public function getPriority(): int
+  {
+    return match ($this) {
+      LogLevel::DEBUG => 0,
+      LogLevel::INFO => 1,
+      LogLevel::WARN => 2,
+      LogLevel::ERROR => 3,
+      LogLevel::FATAL => 4,
+    };
+  }
 }
