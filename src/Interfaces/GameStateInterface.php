1abc3eb9062affe845bf4bf8f98b938262d8e0c2
Andrew Masiye <amasiye313@gmail.com>
2024-05-02 23:04:49 +0200

    feat(interfaces): create GameStateInterface.php

--- src/Interfaces/GameStateInterface.php
+++ src/Interfaces/GameStateInterface.php
@@ -2,7 +2,25 @@
 
 namespace Sendama\Engine\Interfaces;
 
+use Sendama\Engine\Core\Interfaces\CanAwake;
+use Sendama\Engine\Core\Interfaces\CanRender;
+use Sendama\Engine\Core\Interfaces\CanResume;
+use Sendama\Engine\Core\Interfaces\CanStart;
+use Sendama\Engine\Core\Interfaces\CanUpdate;
+
 interface GameStateInterface
 {
+  /**
+   * Updates the game state.
+   *
+   * @return void
+   */
+  public function update(): void;
 
+  /**
+   * Renders the game state.
+   *
+   * @return void
+   */
+  public function render(): void;
 }
