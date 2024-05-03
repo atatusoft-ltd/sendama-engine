abc6de70a25473fba30a19ca957ddbeca9f57e6b
Andrew Masiye <amasiye313@gmail.com>
2024-05-03 18:21:30 +0200

    docs(events/interfaces): add doc comments to SubjectInterface.php

--- src/Events/Interfaces/SubjectInterface.php
+++ src/Events/Interfaces/SubjectInterface.php
@@ -2,7 +2,33 @@
 
 namespace Sendama\Engine\Events\Interfaces;
 
+/**
+ * Interface SubjectInterface. This interface is implemented by classes that want to be observed.
+ *
+ * @package Sendama\Engine\Events\Interfaces
+ */
 interface SubjectInterface
 {
+  /**
+   * This method is called when an observer is added.
+   *
+   * @param ObserverInterface|string $observer The observer that is being added. If a string is passed, it is assumed
+   * to be the name of a class that implements the StaticObserverInterface.
+   */
+  public function addObserver(ObserverInterface|string $observer): void;
 
+  /**
+   * This method is called when an observer is removed.
+   *
+   * @param ObserverInterface|string $observer The observer that is being removed. If a string is passed, it is assumed
+   * to be the name of a class that implements the StaticObserverInterface.
+   */
+  public function removeObserver(ObserverInterface|string $observer): void;
+
+  /**
+   * This method is called when an event is fired.
+   *
+   * @param EventInterface $event The event that is being observed.
+   */
+  public function notify(EventInterface $event): void;
 }
