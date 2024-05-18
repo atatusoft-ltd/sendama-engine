5c7302eb7a8205a2801939ebe06f973cb965f850
Andrew Masiye <amasiye313@gmail.com>
2024-05-18 12:36:34 +0200

    feat(io): update IO module

--- src/IO/GameSerializer.php
+++ src/IO/GameSerializer.php
@@ -2,11 +2,18 @@
 
 namespace Sendama\Engine\IO;
 
+use ReflectionAttribute;
+use ReflectionObject;
+use ReflectionProperty;
+use Sendama\Engine\Core\Behaviours\Attributes\SerializeField;
+use Sendama\Engine\Core\Interfaces\ComponentInterface;
 use Sendama\Engine\Core\Interfaces\SingletonInterface;
 
-class GameObjectSerializer implements SingletonInterface
+class GameSerializer implements SingletonInterface
 {
-  protected static ?GameObjectSerializer $instance;
+  public const string UNGROUPED_HEADER = '__ungrouped';
+
+  protected static ?GameSerializer $instance;
 
   private function __construct()
   {
@@ -20,9 +27,66 @@ class GameObjectSerializer implements SingletonInterface
   {
     if (!self::$instance)
     {
-      self::$instance = new GameObjectSerializer();
+      self::$instance = new GameSerializer();
     }
 
     return self::$instance;
+  }
+
+  /**
+   * Get the serializable fields of a component.
+   *
+   * @param ComponentInterface $component The component.
+   * @return ReflectionProperty[] The serializable fields of the component.
+   */
+  public function getComponentSerializableFields(ComponentInterface $component): array
+  {
+    $header = self::UNGROUPED_HEADER;
+    $fields = [
+      $header => []
+    ];
+
+    $properties = (new ReflectionObject($component))->getProperties();
+
+    foreach($properties as $prop)
+    {
+      $isSerializableField = false;
+
+      if ($attributes = $prop->getAttributes())
+      {
+        $header = ($attributes[0]->newInstance())->text;
+        $isSerializableField = $this->isSerializableField($attributes);
+      }
+
+      if ($prop->isPublic() || $isSerializableField)
+      {
+        $fields[$header][] = $prop;
+      }
+    }
+    return $fields;
+  }
+
+  /**
+   * Check if a field is serializable.
+   *
+   * @param array<ReflectionAttribute> $fieldAttributes The field attributes.
+   * @return bool Whether the field is serializable or not.
+   */
+  private function isSerializableField(array $fieldAttributes): bool
+  {
+    foreach ($fieldAttributes as $attribute)
+    {
+      if ($attribute instanceof ReflectionAttribute)
+      {
+        $instance = $attribute->newInstance();
+
+        if ($instance instanceof SerializeField)
+        {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 }
