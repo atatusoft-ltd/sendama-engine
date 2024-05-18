ae92a205d0807ab17cf279ee0435b8c8ee5f3377
Andrew Masiye <amasiye313@gmail.com>
2024-05-18 12:33:58 +0200

    feat(examples/collector): update The Collector example game

--- examples/collector/assets/Scenes/Level02.php
+++ examples/collector/assets/Scenes/Level02.php
@@ -2,7 +2,21 @@
 
 namespace Sendama\Examples\Collector\Scenes;
 
-class Level02
+use Sendama\Engine\Core\Scenes\AbstractScene;
+
+/**
+ * Class Level02
+ * @package Sendama\Examples\Collector\Scenes
+ */
+class Level02 extends AbstractScene
 {
 
+  /**
+   * @inheritDoc
+   */
+  public function awake(): void
+  {
+    $this->environmentTileMapPath = 'Maps/level02';
+    // TODO: Implement awake() method.
+  }
 }
--- examples/collector/assets/Scripts/Game/LevelManager.php
+++ examples/collector/assets/Scripts/Game/LevelManager.php
@@ -3,11 +3,30 @@
 namespace Sendama\Examples\Collector\Scripts\Game;
 
 use Sendama\Engine\Core\Behaviours\Behaviour;
+use Sendama\Engine\Core\Vector2;
 use Sendama\Engine\IO\Enumerations\KeyCode;
 use Sendama\Engine\IO\Input;
 
+/**
+ * Class LevelManager is responsible for managing the game level.
+ *
+ * @package Sendama\Examples\Collector\Scripts\Game
+ */
 class LevelManager extends Behaviour
 {
+  /**
+   * @var Vector2|array
+   */
+  public Vector2|array $playerStartingPosition = [0, 0];
+
+  public function onStart(): void
+  {
+    if (is_array($this->playerStartingPosition))
+    {
+      $this->playerStartingPosition = Vector2::fromArray($this->playerStartingPosition);
+    }
+  }
+
   /**
    * @inheritDoc
    */
@@ -22,5 +41,19 @@ class LevelManager extends Behaviour
     {
       quitGame();
     }
+  }
+
+  /**
+   * Sets the player's starting position.
+   *
+   * @param Vector2|array $position The player's starting position.
+   */
+  public function setPlayerStartingPosition(Vector2|array $position): void
+  {
+    $this->playerStartingPosition = match (true)
+    {
+      is_array($position) => Vector2::fromArray($position),
+      default => $position
+    };
   }
 }
