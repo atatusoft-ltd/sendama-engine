9b488d51992f7691e64af6dc388b215a4c67b2c8
Andrew Masiye <amasiye313@gmail.com>
2024-05-29 10:36:43 +0200

    feat(events): update Events module

--- src/Events/Enumerations/MenuEventType.php
+++ src/Events/Enumerations/MenuEventType.php
@@ -2,7 +2,20 @@
 
 namespace Sendama\Engine\Events\Enumerations;
 
+/**
+ * An enumeration of menu event types.
+ */
 enum MenuEventType
 {
-
+  case UPDATE;
+  case UPDATE_CONTENT;
+  case RENDER;
+  case ERASE;
+  case ITEM_ACTIVATED;
+  case ITEM_SELECTED;
+  case ITEM_ADDED;
+  case ITEM_REMOVED;
+  case ITEMS_SET;
+  case ITEM_RECEIVED_HORIZONTAL_INPUT;
+  case ITEM_RECEIVED_VERTICAL_INPUT;
 }
