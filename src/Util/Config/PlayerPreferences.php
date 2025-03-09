800840e1c450d0bef11e25d009608c668f481e89
Andrew Masiye <amasiye313@gmail.com>
2025-03-09 05:20:43 +0200

    feat(util): create PlayerPreferences.php

---
+++ src/Util/Config/PlayerPreferences.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sendama\Engine\Util\Config;
+
+use Sendama\Engine\Util\Config\AbstractConfig;
+
+class PlayerPreferences extends AbstractConfig
+{
+
+  /**
+   * @inheritDoc
+   */
+  protected function load(): array
+  {
+    // TODO: Implement load() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function getFilename(): string
+  {
+    // TODO: Implement getFilename() method.
+  }
+}
