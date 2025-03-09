924ef8ed6efd3da248c7f5cd5e1d5b1519957e80
Andrew Masiye <amasiye313@gmail.com>
2025-03-09 05:23:43 +0200

    feat(util): implement PlayerPreferences.php

--- src/Util/Config/PlayerPreferences.php
+++ src/Util/Config/PlayerPreferences.php
@@ -2,7 +2,10 @@
 
 namespace Sendama\Engine\Util\Config;
 
+use Sendama\Engine\Exceptions\NotFoundException;
+use Sendama\Engine\Exceptions\SendamaException;
 use Sendama\Engine\Util\Config\AbstractConfig;
+use Sendama\Engine\Util\Path;
 
 class PlayerPreferences extends AbstractConfig
 {
@@ -12,7 +15,19 @@ class PlayerPreferences extends AbstractConfig
    */
   protected function load(): array
   {
-    // TODO: Implement load() method.
+    $content = file_get_contents($this->filename);
+
+    if (false === $content) {
+      throw new SendamaException("Could not read file: $this->filename");
+    }
+
+    $config = json_decode($content, true);
+
+    if (json_last_error() !== JSON_ERROR_NONE) {
+      throw new SendamaException('Could not decode JSON: ' . json_last_error_msg());
+    }
+
+    return $config;
   }
 
   /**
@@ -20,6 +35,12 @@ class PlayerPreferences extends AbstractConfig
    */
   protected function getFilename(): string
   {
-    // TODO: Implement getFilename() method.
+    $filename = Path::join(Path::getCurrentWorkingDirectory(), 'preferences.json');
+
+    if (!file_exists($filename)) {
+      throw new NotFoundException($filename);
+    }
+
+    return $filename;
   }
 }
